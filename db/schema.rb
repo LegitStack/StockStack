0c0c3095273d8860100a245c125cf7b6290b2018
LegitMiller <jordan.kay@gmail.com>
2015-08-23 14:38:42 -0600

    Added Ajax code

--- db/schema.rb
+++ db/schema.rb
@@ -11,61 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808175256) do
-
-  create_table "messgs", force: true do |t|
-    t.text     "intro"
-    t.text     "message"
-    t.text     "closing"
-    t.boolean  "admin"
-    t.boolean  "master"
-    t.boolean  "coborrower"
-    t.boolean  "borrower"
-    t.boolean  "processor"
-    t.boolean  "realtor"
-    t.boolean  "loanofficer"
-    t.boolean  "escrowofficer"
-    t.boolean  "marketer"
-    t.integer  "progression_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "notes", force: true do |t|
-    t.string   "title"
-    t.text     "comment"
-    t.integer  "user_id"
-    t.integer  "record_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "notifications", force: true do |t|
-    t.integer  "maker_id"
-    t.integer  "user_id"
-    t.integer  "thirdparty_id"
-    t.integer  "record_id"
-    t.string   "changetype"
-    t.string   "change"
-    t.boolean  "seen"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "phases", force: true do |t|
-    t.string   "name"
-    t.integer  "phase_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "phasesteps", force: true do |t|
-    t.datetime "finishedtime"
-    t.integer  "record_id"
-    t.integer  "phase_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140924152757) do
 
   create_table "profiles", force: true do |t|
     t.string   "name"
@@ -78,46 +24,6 @@ ActiveRecord::Schema.define(version: 20150808175256) do
     t.boolean  "assignmail"
     t.string   "status"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "progressions", force: true do |t|
-    t.string   "name"
-    t.integer  "phase_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "records", force: true do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "phone"
-    t.string   "email"
-    t.string   "raemail"
-    t.datetime "receivedate"
-    t.datetime "followupdate"
-    t.string   "progress"
-    t.string   "detailedprogress"
-    t.boolean  "phasemail"
-    t.boolean  "progressmail"
-    t.float    "lopay"
-    t.float    "propay"
-    t.float    "jpay"
-    t.float    "opay"
-    t.boolean  "splitpay"
-    t.integer  "loanofficer_id"
-    t.integer  "processor_id"
-    t.integer  "marketer_id"
-    t.integer  "real_id"
-    t.integer  "escrow_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "steps", force: true do |t|
-    t.integer  "record_id"
-    t.integer  "progression_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
